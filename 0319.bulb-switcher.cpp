c894e9c565a2131c61b85c9a5dafd4c4076adb14
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-05-03 00:25:58 +0300

    added daily uploads

---
+++ 0319.bulb-switcher.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file 0319.bulb-switcher.cpp
+ * @author Ilysa Savitsky (ipsavitsky234@gmail.com)
+ * @brief https://leetcode.com/problems/bulb-switcher/
+ * @version 0.1
+ * @date 2023-05-03
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <cmath>
+
+class Solution {
+  public:
+    int bulbSwitch(int n) {
+        return std::sqrt(n);
+    }
+};
---
+++ 1822.sign-of-product.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file 1822.sign-of-product.cpp
+ * @author Ilya Savitsky (ipsavitsky234@gmail.com)
+ * @brief https://leetcode.com/problems/sign-of-the-product-of-an-array/
+ * @version 0.1
+ * @date 2023-05-03
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <numeric>
+#include <vector>
+
+class Solution {
+  public:
+    int arraySign(std::vector<int> &nums) {
+        return std::reduce(nums.begin(), nums.end(), 0, [](int sgn, int cur) {
+            return sgn *= cur > 0 ? 1 : cur < 0 ? -1 : 0;
+        });
+    }
+};
