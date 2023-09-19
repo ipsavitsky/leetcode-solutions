bf2fe9b8b07ab908d820ce31319f9dbbe2fe08a9
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-09-19 13:15:21 +0100

    Added docstring to 287

--- 0287.find-duplicate-number.cpp
+++ 0287.find-duplicate-number.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file 0287.find-duplicate-number.cpp
+ * @author Ilysa Savitsky (ipsavitsky234@gmail.com)
+ * @brief https://leetcode.com/problems/find-the-duplicate-number
+ * @version 0.1
+ * @date 2023-09-19
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 #include <algorithm>
 
 class Solution {
