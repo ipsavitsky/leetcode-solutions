572a24859067987cfc0de26360f382fb5340514d
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-09-21 14:58:39 +0100

    Study plan solutions added

---
+++ 0283.move-zeroes.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file 0283.move-zeroes.cpp
+ * @author Ilysa Savitsky (ipsavitsky234@gmail.com)
+ * @brief https://leetcode.com/problems/move-zeroes/
+ * @version 0.1
+ * @date 2023-09-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <vector>
+
+class Solution {
+  public:
+    void moveZeroes(std::vector<int> &nums) {
+        using v_it = std::vector<int>::iterator;
+        v_it j = nums.begin();
+        for (v_it i = nums.begin(); i != nums.end(); ++i) {
+            if (*i != 0) {
+                std::iter_swap(i, j);
+                j++;
+            }
+        }
+    }
+};
---
+++ 0459.repeated-string-pattern.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file 0459.repeated-string-pattern.cpp
+ * @author Ilya Savitsky (ipsavitsky234@gmail.com)
+ * @brief https://leetcode.com/problems/repeated-substring-pattern/
+ * @version 0.1
+ * @date 2023-09-21
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+#include <regex>
+#include <string>
+
+class Solution {
+  public:
+    bool repeatedSubstringPattern(std::string s) {
+        for (int i = 1; i <= s.size() / 2; ++i) {
+            if (s.size() % i != 0) {
+                continue;
+            }
+            std::string s_sub_s = s.substr(0, i);
+            std::regex r("(" + s_sub_s + ")+");
+            if (std::regex_match(s, r)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
