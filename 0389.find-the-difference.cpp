e8dcc6d0ef119b7ca9fef578f200cfe689ef0b6f
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-09-26 12:59:05 +0100

    Added dailies for 26.09 and 25.09

--- 0389.find-the-difference.cpp
+++ 0389.find-the-difference.cpp
@@ -3,21 +3,19 @@
  * @author Ilya Savitsky (ipsavitsky234@gmail.com)
  * @brief https://leetcode.com/problems/find-the-difference/
  * @version 0.1
- * @date 2023-09-19
+ * @date 2023-09-25
  *
  * @copyright Copyright (c) 2023
  *
  */
-#include <algorithm>
+#include <numeric>
 #include <string>
 
 class Solution {
   public:
     char findTheDifference(std::string s, std::string t) {
-        std::sort(s.begin(), s.end());
-        std::sort(t.begin(), t.end());
-        std::string::iterator it =
-            std::mismatch(s.begin(), s.end(), t.begin(), t.end()).second;
-        return *it;
+        int sum_s = std::accumulate(s.begin(), s.end(), 0);
+        int sum_t = std::accumulate(t.begin(), t.end(), 0);
+        return sum_t - sum_s;
     }
 };
---
+++ 1081.smallest-subsequence-of-distinct-characters.cpp
@@ -0,0 +1,50 @@
+/**
+ * @file 1081.smallest-subsequence-of-distinct-characters.cpp
+ * @author Ilya Savitsky (ipsavitsky234@gmail.com)
+ * @brief https://leetcode.com/problems/smallest-subsequence-of-distinct-characters
+ * @version 0.1
+ * @date 2023-09-26
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <algorithm>
+#include <deque>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+class Solution {
+  public:
+    std::string smallestSubsequence(std::string s) {
+        using s_it = std::string::iterator;
+        using uchar = unsigned char;
+        std::unordered_map<uchar, s_it> last_usage;
+
+        for (s_it cur = s.begin(); cur != s.end(); ++cur) {
+            last_usage[*cur] = cur;
+        }
+
+        std::unordered_set<uchar> visited;
+        std::deque<s_it> res;
+
+        for (s_it cur = s.begin(); cur != s.end(); ++cur) {
+            if (!visited.contains(*cur)) {
+                while (!res.empty() && *(res.back()) > *cur &&
+                       cur < last_usage[*(res.back())]) {
+                    visited.erase(*(res.back()));
+                    res.pop_back();
+                }
+                res.push_back(cur);
+                visited.insert(*cur);
+            }
+        }
+
+        std::string res_s;
+        res_s.reserve(s.size());
+        std::transform(res.begin(), res.end(), std::back_inserter(res_s),
+                       [](const s_it &cur) { return *cur; });
+        
+        return res_s;
+    }
+};
